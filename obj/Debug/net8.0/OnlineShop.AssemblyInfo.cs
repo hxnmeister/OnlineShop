d91986b8e9ae70a3550d59bd182ade71a44a999f
Valerii <valeriy.stronskiy@gmail.com>
2024-07-11 14:47:34 +0300

    Removed HomeController

--- obj/Debug/net8.0/OnlineShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a164f796f973f92146fabaf59efe3bf176692a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
